5ba8cd515d1b9bc48933dcfcfdf10e05d5abae81
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2018-07-30 14:56:00 +0900

    Offsetクラス追加

---
+++ Offset.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Excel.Helpers
+{
+    public class Offset
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+    }
+}
